2efbb332b13d9151a32c316ce3a0ae189a19215c
jqlnet <toniliang@cpp.edu>
2025-09-20 01:03:34 -0700

    updated timer settings

--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
-using TMPro; 
+using TMPro;
 
 public class InGameUI : MonoBehaviour
 {
     public TextMeshProUGUI foodText;
-     public TextMeshProUGUI timerText;
+    public TextMeshProUGUI timerText;
     [SerializeField] PlayerMovement player;
 
     private float timer = 0f;
     void Update()
     {
-
         timer += Time.deltaTime;
-        timerText.text = $": {timer:F2} s";
+        int minutes = Mathf.FloorToInt(timer / 60f);
+        int seconds = Mathf.FloorToInt(timer % 60f);
+        int milliseconds = Mathf.FloorToInt((timer * 1000f) % 1000f);
+        timerText.text = $"Timer: {minutes:D2}:{seconds:D2}:{milliseconds:D3}";
 
         foodText.text = ": " + player.foodsCollected.ToString() + " / 9";
     }
