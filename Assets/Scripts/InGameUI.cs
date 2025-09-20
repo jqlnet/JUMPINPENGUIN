597a17758b42799cbe34635becef564b5ebfb867
jqlnet <toniliang@cpp.edu>
2025-09-20 01:08:40 -0700

    added milliseconds

--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -14,7 +14,7 @@ public class InGameUI : MonoBehaviour
         int minutes = Mathf.FloorToInt(timer / 60f);
         int seconds = Mathf.FloorToInt(timer % 60f);
         int milliseconds = Mathf.FloorToInt((timer * 1000f) % 1000f);
-        timerText.text = $"Timer: {minutes:D2}:{seconds:D2}:{milliseconds:D3}";
+        timerText.text = $"{minutes:D2}:{seconds:D2}:{milliseconds:D3}";
 
         foodText.text = ": " + player.foodsCollected.ToString() + " / 9";
     }
